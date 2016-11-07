4a2f2a231bce9912a2c0cabb9c934461d3f14232
ao1415 <taku31126816@yahoo.co.jp>
2016-11-07 18:27:22 +0900

    ボムデータ表示

--- FallSweeper/Main.cpp
+++ FallSweeper/Main.cpp
@@ -1,14 +1,70 @@
-﻿
-# include <Siv3D.hpp>
+﻿#include <Siv3D.hpp>
 
-void Main()
-{
-	const Font font(30);
+const int StageWidth = 16;
+const int BlockSize = 24;
+
+const int BombNumber = 10;
+
+class Control {
+public:
+
+	Control() {
+		stage.resize(10);
+		for (auto& s : stage) s.resize(StageWidth);
+
+		for (size_t y = 0; y < stage.size(); y++)
+		{
+			const auto& column = getColumn();
+			for (size_t x = 0; x < StageWidth; x++)
+				stage[y][x] = column[x];
+		}
+
+	}
+
+	void update() {
+
+		if (Input::KeyEnter.clicked)
+		{
+			const auto column = getColumn();
+			stage.push_back(column);
+		}
+
+	}
+
+	void draw() const {
+		for (size_t y = 0; y < stage.size(); y++)
+		{
+			for (size_t x = 0; x < stage[y].size(); x++)
+			{
+				Rect(x*BlockSize, y*BlockSize, BlockSize, BlockSize).draw(Palette::Gray);
+				Rect(x*BlockSize, y*BlockSize, BlockSize, BlockSize).drawFrame(1, 0, Palette::White);
+				PutText(stage[y][x]).from(x*BlockSize, y*BlockSize);
+			}
+		}
+	}
+
+private:
+
+	Array<Array<int>> stage;
+
+	Array<int> getColumn() const {
+
+		Array<int> column;
+		for (int i = 0; i < StageWidth; i++)
+			column.push_back(Random() <= 0.15625 ? BombNumber : 0);
+
+		return column;
+	}
+
+};
+
+void Main() {
+
+	Control control;
 
 	while (System::Update())
 	{
-		font(L"ようこそ、Siv3D の世界へ！").draw();
-
-		Circle(Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
+		control.update();
+		control.draw();
 	}
 }
